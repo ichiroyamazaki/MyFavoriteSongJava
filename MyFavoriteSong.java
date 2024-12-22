2c0ffeb25f2b4fd6bb37cef310c45f66c03673ad
Ichiro Yamazaki <16699009+ichiroyamazaki@users.noreply.github.com>
2024-12-23 00:22:02 +0800

    Create MyFavoriteSong.java

---
+++ MyFavoriteSong.java
@@ -0,0 +1,18 @@
+/*
+   Student: Ichiro Yamazaki
+   Student No.: xxxxxxxxxxx
+   Song Title: Joji - Glimpse of Us
+*/
+public class MyFavoriteSong 
+{
+    public static void main(String args[]) 
+    {
+        System.out.println("'Cause sometimes I look in her eyes, and that's where I find a glimpse of us");
+        System.out.println("And I try to fall for her touch, but I'm thinking of the way it was");
+        System.out.println("Said, ' I'm fine' and said, 'I moved on' ");
+        System.out.println("I'm only here passing time in her arms. Hoping I'll find a glimpse of us.");
+        /* This song means that is all about a guy reminiscing about he and his girlfriend Used to be and what the relationship Used to be like.
+        Anyone who has been in a long-term relationship knows that the honeymoon phase doesn't last forever. 
+        */
+    }
+}
